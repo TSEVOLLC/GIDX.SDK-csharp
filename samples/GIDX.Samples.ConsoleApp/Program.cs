cd8d8f6253b8df51e6db421cd3c41e9debe89518
Nick Miller <nick.miller@tsevo.com>
2024-03-29 12:20:55 -0500

    Added sample console app

---
+++ samples/GIDX.Samples.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using GIDX.SDK;
+
+namespace GIDX.Samples.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
