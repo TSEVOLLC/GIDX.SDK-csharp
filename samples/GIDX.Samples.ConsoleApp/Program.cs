bb8f3348e61f8c8d26d11dc68ced049abbcab1bd
Nick Miller <nick.miller@tsevo.com>
2024-03-29 15:41:10 -0500

    Set up dependency injection for GIDXClient.

--- samples/GIDX.Samples.ConsoleApp/Program.cs
+++ samples/GIDX.Samples.ConsoleApp/Program.cs
@@ -1,12 +1,54 @@
 ﻿using GIDX.SDK;
+using GIDX.SDK.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace GIDX.Samples.ConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        async static Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddTransient<Program>();
+
+            builder.Services.AddHttpClient(GIDX.SDK.GIDXClient.GIDXHttpClientName);
+            
+            var credentials = new MerchantCredentials
+            {
+                MerchantID = "",
+                ApiKey = "",
+                ActivityTypeID = "",
+                ProductTypeID = "",
+                DeviceTypeID = ""
+            };
+            builder.Services.AddSingleton(credentials);
+            builder.Services.AddScoped(sp =>
+            {
+                var domain = "https://api.gidx-service.in";
+                //var domain = "https://api.gidx-service.com";
+                var version = "3.0";
+                var credentials = sp.GetRequiredService<MerchantCredentials>();
+                var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+                return new GIDXClient(credentials, domain, version, httpClientFactory);
+            });
+
+            using IHost host = builder.Build();
+            using IServiceScope scope = host.Services.CreateScope();
+            scope.ServiceProvider.GetRequiredService<Program>().Run();
+            await host.RunAsync();
+        }
+
+        private readonly GIDXClient gidxClient;
+
+        public Program(GIDXClient gidxClient)
+        {
+            this.gidxClient = gidxClient;
+        }
+
+        public void Run()
+        {
+            //gidxClient.DirectCashier.CreateSessionAsync()
         }
     }
 }
