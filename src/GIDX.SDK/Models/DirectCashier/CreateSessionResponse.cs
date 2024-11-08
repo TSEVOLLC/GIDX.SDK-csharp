8b85d7297c4a005c6de58137b54d258c7d546bef
Nick Miller <nick.miller@tsevo.com>
2024-11-08 14:59:34 -0600

    Add SessionID to DirectCashier CreateSessionResponse

--- src/GIDX.SDK/Models/DirectCashier/CreateSessionResponse.cs
+++ src/GIDX.SDK/Models/DirectCashier/CreateSessionResponse.cs
@@ -10,6 +10,7 @@ namespace GIDX.SDK.Models.DirectCashier
     public class CreateSessionResponse : ResponseBase, IReasonCodes
     {
         public string MerchantTransactionID { get; set; }
+        public string SessionID { get; set; }
         public decimal SessionScore { get; set; }
         public string SessionStatusCode { get; set; }
         public string SessionStatusMessage { get; set; }
