29ae17de0318414c26158283883eb93f0142ac72
Nick Miller <nick.miller@tsevo.com>
2025-09-24 07:04:08 -0500

    Added missing ThreeDS fields

--- src/GIDX.SDK/Models/DirectCashier/ThreeDS.cs
+++ src/GIDX.SDK/Models/DirectCashier/ThreeDS.cs
@@ -6,8 +6,11 @@ namespace GIDX.SDK.Models.DirectCashier
 {
     public class ThreeDS
     {
+        public bool? Enabled { get; set; }
+        public string Provider { get; set; }
         public string CAVV { get; set; }
         public string ECI { get; set; }
+        public string TransactionID { get; set; }
         public string ACSTransactionID { get; set; }
         public string DSTransactionID { get; set; }
     }
