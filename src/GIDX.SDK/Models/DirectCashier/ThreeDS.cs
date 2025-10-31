023d7bf0a9a3104ffeabd9a08ff88dd03aa2ba89
Nick Miller <nick.miller@tsevo.com>
2025-10-31 09:28:49 -0500

    Added missing Approvely Rapid device details from ThreeDS model

--- src/GIDX.SDK/Models/DirectCashier/ThreeDS.cs
+++ src/GIDX.SDK/Models/DirectCashier/ThreeDS.cs
@@ -13,5 +13,13 @@ namespace GIDX.SDK.Models.DirectCashier
         public string TransactionID { get; set; }
         public string ACSTransactionID { get; set; }
         public string DSTransactionID { get; set; }
+
+        //Device details for Approvely Rapid
+        public decimal? ColorDepth { get; set; }
+        public decimal? ScreenHeight { get; set; }
+        public decimal? ScreenWidth { get; set; }
+        public decimal? TimeZone { get; set; }
+        public string UserAgent { get; set; }
+        public string DeviceID { get; set; }
     }
 }
